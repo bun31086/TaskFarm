2631ecf0f169be1eb21d25600f8731fab57ac31d
STD\s20227016 <20227016-yumotoraiki@hcs.ac.jp>
2024-03-27 16:52:09 +0900

    MoveCheckなど作成

---
+++ Assets/Scripts/Players/PlayerDataClass.cs
@@ -0,0 +1,28 @@
+// ---------------------------------------------------------  
+// PlayerData.cs  
+// プレイヤーの情報管理
+// 作成日:  3/27~
+// 作成者:  湯元来輝
+// ---------------------------------------------------------  
+using UnityEngine;
+using System.Collections;
+/// <summary>
+/// プレイヤーの情報管理
+/// </summary>
+[CreateAssetMenu(fileName ="PlayerData",menuName = "ScriptableObjects/PlayerData")]
+public class PlayerDataClass : ScriptableObject
+{
+
+    [SerializeField,Tooltip("プレイヤーの移動速度")]
+    private float _speed = 5f;
+
+
+    public float Speed
+    {
+
+        get => _speed;
+    
+    }
+
+
+}
--- Assets/Scripts/Players/PlayerManagerClass.cs
+++ Assets/Scripts/Players/PlayerManagerClass.cs
@@ -1,11 +1,12 @@
 // ---------------------------------------------------------  
 // PlayerManagerClass.cs  
 // プレイヤー管理クラス
-// 作成日:  
-// 作成者:  
+// 作成日:  3/27~
+// 作成者:  湯元来輝
 // ---------------------------------------------------------  
 using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
 /// <summary>
 /// プレイヤー管理
 /// </summary>
@@ -14,8 +15,27 @@ public class PlayerManagerClass : MonoBehaviour
 
     #region 変数  
 
-    private PlayerStateMachineClass _playerStateMachine;
-    private PlayerInputClass _playerInput;
+    [Header("スクリプタブルオブジェクト")]
+    [SerializeField, Tooltip("プレイヤーのデータ")]
+    private PlayerDataClass _playerData = default;
+
+    /// <summary>
+    /// プレイヤーステートを変更するクラスのインスタンス
+    /// </summary>
+    private PlayerStateMachineClass _playerStateMachine = new PlayerStateMachineClass();
+    /// <summary>
+    /// 移動を確認するクラスのインスタンス
+    /// </summary>
+    private MoveCheckClass<Transform> _moveCheck = default;
+
+
+
+
+
+    /// <summary>
+    /// プレイヤーインプットクラスのインスタンス
+    /// </summary>
+    private PlayerInputClass _playerInput = new PlayerInputClass();
 
     #endregion
 
@@ -25,31 +45,47 @@ public class PlayerManagerClass : MonoBehaviour
 
     #region メソッド  
 
-    /// <summary>  
-    /// 初期化処理  
-    /// </summary>  
-    void Awake()
-     {
-     }
-  
-     /// <summary>  
-     /// 更新前処理  
-     /// </summary>  
-     void Start ()
-     {
-        _playerStateMachine = new PlayerStateMachineClass();
-        _playerInput = GetComponent<PlayerInputClass>();
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Awake()
+    {
 
+        //自分のインスタンスをコンストラクタに渡し生成
+        _moveCheck = new MoveCheckClass<Transform>(this.transform);
+
+    }
+
+    /// <summary>  
+    /// 更新前処理  
+    /// </summary>  
+    private void Start ()
+    {
+        
+        //初期ステータスに変更
         _playerStateMachine.ChangeState(new IdleClass(_playerStateMachine, _playerInput));
+        //移動先確認
+        _moveCheck.Check();
 
     }
 
     /// <summary>  
     /// 更新処理  
     /// </summary>  
-    void Update ()
+    private void Update ()
     {
+
         _playerStateMachine.Update();
+
+    }
+
+    /// <summary>
+    /// 移動の値を取得
+    /// </summary>
+    /// <param name="context">入力値</param>
+    public void OnMove(InputAction.CallbackContext context)
+    {
+
 
     }
 
