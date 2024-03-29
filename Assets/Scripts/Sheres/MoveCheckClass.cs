a0f4882aeda1e292b2e0c969cb3ef88aa35ecd98
STD\s20227016 <20227016-yumotoraiki@hcs.ac.jp>
2024-03-29 19:34:49 +0900

    プレイヤーコントローラー仮完成

--- Assets/Scripts/Sheres/MoveCheckClass.cs
+++ Assets/Scripts/Sheres/MoveCheckClass.cs
@@ -25,11 +25,11 @@ public class MoveCheckClass<T> where T : Transform
     /// <summary>
     /// コンストラクタ
     /// </summary>
-    /// <param name="callTrans">クラス</param>
+    /// <param name="callTrans">呼んだクラスがアタッチされているトランスフォーム</param>
     public MoveCheckClass(T callTrans)
     {
 
-        //呼び出したクラスのインスタンス取得
+        //呼んだクラスがアタッチされているトランスフォーム取得
         this._callTrans = callTrans;
 
     }
@@ -44,20 +44,18 @@ public class MoveCheckClass<T> where T : Transform
     /// <returns>Rayの情報</returns>
     public RaycastHit[] Check()
     {
-
         //Rayに当たった全ての情報が入る
         RaycastHit[] hits = default;
         //倍率をかけたサイズ取得
-        Vector3 size = _callTrans.localScale　* _magnification;
+        Vector3 size = _callTrans.lossyScale * _magnification;
         //サイズのYを呼び出したクラスのトランスフォームに変える
-        size.y = _callTrans.localScale.y;
-        Debug.Log(size);
+        size.y = _callTrans.lossyScale.y;
         //開始位置
         Vector3 pos = _callTrans.position + _callTrans.forward * size.z / 2;
         //向き
         Quaternion dire = _callTrans.rotation;
         //距離
-        float dist = _callTrans.localScale.z;
+        float dist = size.z / 2;
         //プレイヤーのLayerMask取得
         int layer = LayerMask.NameToLayer("Player");
         //演算子を反転
