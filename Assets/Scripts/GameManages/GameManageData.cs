07318ff7f2f339dea932e06d5acfe3f315faa3ab
STD\s20227016 <20227016-yumotoraiki@hcs.ac.jp>
2024-04-01 11:17:31 +0900

    ムーブチェックに関係のあるクラスと、移動系クラスに関係のあるクラスの修正

--- Assets/Scripts/GameManages/GameManageData.cs
+++ Assets/Scripts/GameManages/GameManageData.cs
@@ -1,48 +1,30 @@
 // ---------------------------------------------------------  
 // GameManageData.cs  
 // ゲームのデータ管理
-// 作成日:  
-// 作成者:  
+// 作成日:  4/1
+// 作成者:  湯元来輝
 // ---------------------------------------------------------  
 using UnityEngine;
 using System.Collections;
 /// <summary>
 /// ゲームのデータ管理
 /// </summary>
+[CreateAssetMenu(fileName ="GameManagerData",menuName = "ScriptableObjects/GamaManagerData")]
 public class GameManageData : ScriptableObject
 {
-  
-    #region 変数  
-  
-    #endregion
-  
-    #region プロパティ  
-  
-    #endregion
-  
-    #region メソッド  
-  
-     /// <summary>  
-     /// 初期化処理  
-     /// </summary>  
-     void Awake()
-     {
-     }
-  
-     /// <summary>  
-     /// 更新前処理  
-     /// </summary>  
-     void Start ()
-     {
-  
-     }
-  
-     /// <summary>  
-     /// 更新処理  
-     /// </summary>  
-     void Update ()
-     {
-     }
-  
-    #endregion
+
+    [Header("ゲームルール")]
+    [SerializeField, Tooltip("目標金額")]
+    private float _targetMonay = default;
+    [SerializeField,Tooltip("制限時間")]
+    private float _timeLimet = default;
+    //プロパティ
+    public float TargetMonay
+    {
+        get => _targetMonay;
+    }
+    public float TimeLimet
+    {
+        get => _timeLimet;
+    }
 }
