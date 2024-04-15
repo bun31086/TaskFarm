c7311a7ee4bd9bc13e227251e0012433f67b3846
STD\s20227066 <lo8e6Vcp>
2024-04-15 14:41:50 +0900

    動物インターフェース更新

--- Assets/Scripts/Moves/RunClass.cs
+++ Assets/Scripts/Moves/RunClass.cs
@@ -36,8 +36,8 @@ public class RunClass : IMoveState
     #region メソッド
     public void Enter()
     {
-        Debug.Log("走り開始");
-        Debug.Log(_moveVector);
+       // Debug.Log("走り開始");
+        //Debug.Log(_moveVector);
         _animator.SetBool("IsRun", true);
         //移動する方向に向きを変える
         Vector3 lookPos = _moveVector + _rigidbody.transform.position;
